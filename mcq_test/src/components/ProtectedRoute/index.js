b2a6107af56fb500f159e3c74f818699fb9bb3f4
FaizullahSayyed <141986421+FaizullahSayyed@users.noreply.github.com>
2024-09-05 10:06:01 +0000

    adding protected route

--- mcq_test/src/components/ProtectedRoute/index.js
+++ mcq_test/src/components/ProtectedRoute/index.js
@@ -1,14 +1,9 @@
 import Cookies from 'js-cookie'
-import {Navigate, Route} from 'react-router-dom'
+import {Navigate} from 'react-router-dom'
 
-const ProtectedRoute = props =>{
+const ProtectedRoute = ({element, ...rest}) =>{
     const jwtToken = Cookies.get("jwtToken")
-    if(jwtToken !== undefined){
-        return <Navigate to="/test" />
-    }else{
-        console.log(props)
-        return <Route {...props} />
-    }
+    return jwtToken ? element : <Navigate to="/login" />
 }
 
 export default ProtectedRoute

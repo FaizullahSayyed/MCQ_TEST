7539a7862c5dd7df90c6141825b6f6c36acf3ee1
FaizullahSayyed <141986421+FaizullahSayyed@users.noreply.github.com>
2024-09-05 09:40:27 +0000

    adding protected route

--- mcq_test/src/App.js
+++ mcq_test/src/App.js
@@ -3,11 +3,13 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './components/Home'
 import NotFound from './components/NotFound';
 import Login from './components/Login';
+import Test from './components/Test';
 
 const App = () =>(
   <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path='/test' element={<Test />} />
       <Route path="*" element={<NotFound />} />
   </Routes>
 )

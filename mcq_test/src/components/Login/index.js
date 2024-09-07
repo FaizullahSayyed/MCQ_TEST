c1c9ad06a29ef168769b4e8ea8a788f6f97adb6a
FaizullahSayyed <faizs7412@gmail.com>
2024-09-07 11:30:17 +0530

    updated Login UI

--- mcq_test/src/components/Login/index.js
+++ mcq_test/src/components/Login/index.js
@@ -1,5 +1,42 @@
+import { useState } from 'react'
+
+import {BGContainer, ButtonContainer, Form, FormContainer, FormHeading, InputContainer, InputField, Label, LoginButton, SignupButton} from './styledComponents'
+
 const Login = () =>{
-    return <h1>Login</h1>
+    const [username, setName] = useState("")
+    const [password, setPassword] = useState("")
+    const [userDetails, setUserDetails] = useState({})
+
+    const onChangeUsername = e => setName(e.target.value)
+
+    const onChangePassword = e => setPassword(e.target.value)
+
+    const onSubmitForm = e => {
+        e.preventDefault()
+        setUserDetails({username, password})
+    }
+
+    return (
+    <BGContainer>
+        <FormContainer>
+            <Form onSubmit={onSubmitForm}>
+            <InputContainer>
+                <FormHeading>Login</FormHeading>
+                <Label htmlFor='username'>USERNAME</Label>
+                <InputField type="text" id="username" value={username} onChange={onChangeUsername}/>
+            </InputContainer>
+            <InputContainer>
+                <Label htmlFor='password'>PASSWORD</Label>
+                <InputField type="password" id="password" value={password} onChange={onChangePassword}/>
+            </InputContainer>
+            <ButtonContainer>
+                <LoginButton type="submit">Login</LoginButton>
+                <SignupButton>Signup</SignupButton>
+            </ButtonContainer>
+            </Form>
+        </FormContainer>
+    </BGContainer>
+    )
 }
 
 export default Login

342f95fb64980ffc10a5dd8178c43c08c2d892ac
FaizullahSayyed <faizs7412@gmail.com>
2024-09-10 16:11:10 +0530

    adding signup form

--- mcq_test/src/components/SignupForm/index.js
+++ mcq_test/src/components/SignupForm/index.js
@@ -1,19 +1,42 @@
-<SignUpContainer>
-                            <Form>
-                                <InputContainer>
-                                    <Label htmlFor='fullName'>Full Name</Label>
-                                    <InputContainer placeholder='Full Name' id='fullName' value={fullName}/>
-                                </InputContainer>
-                                <InputContainer>
-                                    <Label htmlFor='classOptions'></Label>
-                                   <StyledSelect id="classOptions" value={selectedOption} onChange={onChangeOption}>
-                                        {
-                                            options.map(option=>
-                                            <StyledOption key={option.value} value={option.value}>
-                                                {option.label}
-                                            </StyledOption>)
-                                        }
-                                   </StyledSelect>
-                                </InputContainer>
-                            </Form>
-                        </SignUpContainer>
+import {useState} from 'react'
+
+const SignupForm = () => {
+    const [fullName, setFullName] = useState("")
+    const [selectedOption, selectOption] = useState("")
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("")
+    const [phoneNo, serPhoneNo] = useState("")
+
+    const onChangeOption = e => selectOption(e.target.value)
+
+    const onChangePhone = e => setPhoneNo(e.targer.value)
+
+    const onChangeSignupUsername = e => 
+
+    return (
+        <SignUpContainer>
+            <Form>
+                <InputContainer>
+                    <Label htmlFor='fullName'>Full Name</Label>
+                    <InputContainer placeholder='Full Name' id='fullName' value={fullName}/>
+                </InputContainer>
+                <InputContainer>
+                    <Label htmlFor='classOptions'></Label>
+                    <StyledSelect id="classOptions" value={selectedOption} onChange={onChangeOption}>
+                        {
+                            options.map(option=>
+                            <StyledOption key={option.value} value={option.value}>
+                                {option.label}
+                            </StyledOption>)
+                        }
+                    </StyledSelect>
+                </InputContainer>
+            </Form>
+        </SignUpContainer>
+    )
+}
+
+export default SignupForm
+
+
